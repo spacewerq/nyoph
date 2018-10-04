a654576cea99b2ae6268c3f20f16421033a3c9d9
Ron G <2109530+ronnieguha@users.noreply.github.com>
2018-10-04 08:03:56 -0400

    Update site.js

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-51838815-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-51838815-1');
+</script>
+
